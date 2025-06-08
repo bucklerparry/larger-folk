5b3fe4619a862dee1c30cf702ff0366551077f32
BucklerParry <joebloggbro@gmail.com>
2025-06-07 22:30:14 -0400

    castes / callings now work with weight gain

--- larger_folk/LargerFolk_CasteWeightStages.cs
+++ larger_folk/LargerFolk_CasteWeightStages.cs
@@ -81,9 +81,228 @@ public class LargerFolk_CasteWeightStages : IPlayerMutator
                 player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/first_child_of_the_hearth_fat.png";
             }
 
+             // True Kin Castes
+            else if (myRender.Tile == "creatures/caste_1.bmp" || myRender.Tile == "bucklerparry_largerfolk/horticulturalist_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/horticulturalist_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_1.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/horticulturalist_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_2.bmp" || myRender.Tile == "bucklerparry_largerfolk/priest_of_all_suns_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/priest_of_all_suns_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_2.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/priest_of_all_suns_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_3.bmp" || myRender.Tile == "bucklerparry_largerfolk/priest_of_all_moons_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/priest_of_all_moons_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_3.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/priest_of_all_moons_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_4.bmp" || myRender.Tile == "bucklerparry_largerfolk/syzygyrior_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/syzygyrior_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_4.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/syzygyrior_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_5.bmp" || myRender.Tile == "bucklerparry_largerfolk/artifex_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/artifex_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_5.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/artifex_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_6.bmp" || myRender.Tile == "bucklerparry_largerfolk/consul_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/consul_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_6.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/consul_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_7.bmp" || myRender.Tile == "bucklerparry_largerfolk/praetorian_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/praetorian_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_7.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/praetorian_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_8.bmp" || myRender.Tile == "bucklerparry_largerfolk/eunuch_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/eunuch_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_8.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/eunuch_fat.png";
+            }
+            else if (myRender.Tile =="creatures/caste_9.bmp" || myRender.Tile == "bucklerparry_largerfolk/hearthchild_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/hearthchild_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_9.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/hearthchild_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_10.bmp" || myRender.Tile == "bucklerparry_largerfolk/child_of_the_wheel_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/child_of_the_wheel_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_10.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/child_of_the_wheel_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_11.bmp" || myRender.Tile == "bucklerparry_largerfolk/deepchild_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/deepchild_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_11.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/deepchild_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_12.bmp" || myRender.Tile == "bucklerparry_largerfolk/fuming_godchild_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/fuming_godchild_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_12.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/fuming_godchild_fat.png";
+            }
+
+            // Mutated Human Callings
+            else if (myRender.Tile == "creatures/caste_13.bmp" || myRender.Tile == "bucklerparry_largerfolk/apostle_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/apostle_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_13.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/apostle_fat.png";
+            }
+
+            else if (myRender.Tile == "creatures/caste_14.bmp" || myRender.Tile == "bucklerparry_largerfolk/arconaut_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/arconaut_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_14.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/arconaut_fat.png";
+            }
+
+            else if (myRender.Tile == "creatures/caste_15.bmp" || myRender.Tile == "bucklerparry_largerfolk/greybeard_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/greybeard_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_15.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/greybeard_fat.png";
+            }
+
+            else if (myRender.Tile == "creatures/caste_16.bmp" || myRender.Tile == "bucklerparry_largerfolk/gunslinger_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/gunslinger_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_16.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/gunslinger_fat.png";
+            }
+
+            else if (myRender.Tile == "creatures/caste_17.bmp" || myRender.Tile == "bucklerparry_largerfolk/marauder_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/marauder_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_17.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/marauder_fat.png";
+            }
+            
+            else if (myRender.Tile == "creatures/caste-pilgrim.png" || myRender.Tile == "bucklerparry_largerfolk/pilgrim_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/pilgrim_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste-pilgrim.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/pilgrim_fat.png";
+            }
+
+            else if (myRender.Tile == "creatures/caste_19.bmp" || myRender.Tile == "bucklerparry_largerfolk/nomad_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/nomad_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_19.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/nomad_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_20.bmp" || myRender.Tile == "bucklerparry_largerfolk/scholar_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/scholar_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_20.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/scholar_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_21.bmp" || myRender.Tile == "bucklerparry_largerfolk/tinker_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/tinker_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_21.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/tinker_fat.png";
+            }
+
+            else if (myRender.Tile == "creatures/caste_22.bmp" || myRender.Tile == "bucklerparry_largerfolk/caste_warden_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/caste_warden_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_22.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/caste_warden_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_23.bmp" || myRender.Tile == "bucklerparry_largerfolk/water_merchant_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/water_merchant_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_23.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/water_merchant_fat.png";
+            }
+            else if (myRender.Tile == "creatures/caste_24.bmp" || myRender.Tile == "bucklerparry_largerfolk/farmer1_fat.png")
+            {
+                player.GetPart<Render>().Tile = "bucklerparry_largerfolk/farmer1_fat.png";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_0 = "creatures/caste_24.bmp";
+                player.GetPart<LargerFolk_WeightStageRender>().WeightStage_1 = "bucklerparry_largerfolk/farmer1_fat.png";
+            }
+
             player.GetPart<LargerFolk_WeightGain>().TotalCalories = 1000;
             player.GetPart<LargerFolk_WeightGain>().InitializeWeightStage();
         }
 
     }
 }
+// "creatures/caste_1.bmp"
+// "bucklerparry_largerfolk/horticulturalist_fat.png"
+
+// "bucklerparry_largerfolk/priest_of_all_suns_fat.png"
+
+// "bucklerparry_largerfolk/priest_of_all_moons_fat.png"
+
+// "bucklerparry_largerfolk/syzygyrior_fat.png"
+
+// "bucklerparry_largerfolk/artifex_fat.png"
+
+// "bucklerparry_largerfolk/consul_fat.png"
+
+// "bucklerparry_largerfolk/praetorian_fat.png"
+
+// "bucklerparry_largerfolk/eunuch_fat.png"
+
+// "bucklerparry_largerfolk/hearthchild_fat.png"
+
+// "bucklerparry_largerfolk/child_of_the_wheel_fat.png"
+
+// "bucklerparry_largerfolk/deepchild_fat.png"
+
+// "creatures/caste_12.bmp"
+// "bucklerparry_largerfolk/fuming_godchild_fat.png"
+
+
+
+
+            // "creatures/caste_13.bmp"
+            // "bucklerparry_largerfolk/apostle_fat.png"
+            
+            // "creatures/caste_14.bmp"
+            // "bucklerparry_largerfolk/arconaut_fat.png"
+
+            // "creatures/caste_15.bmp"
+            // "bucklerparry_largerfolk/greybeard_fat.png"
+
+            // "creatures/caste_16.bmp"
+            // "bucklerparry_largerfolk/gunslinger_fat.png"
+
+            // "creatures/caste_17.bmp"
+            // "bucklerparry_largerfolk/marauder_fat.png"
+
+            // "creatures/caste-pilgrim.png"
+            // "bucklerparry_largerfolk/pilgrim_fat.png"
+
+            // "creatures/caste_19.bmp"
+            // "bucklerparry_largerfolk/nomad_fat.png"
+
+            // "creatures/caste_20.bmp"
+            // "bucklerparry_largerfolk/scholar_fat.png"
+
+            // "creatures/caste_21.bmp"
+            // "bucklerparry_largerfolk/tinker_fat.png"
+
+            // "creatures/caste_22.bmp"
+            // "bucklerparry_largerfolk/caste_warden_fat.png"
+
+            // "creatures/caste_23.bmp"
+            // "bucklerparry_largerfolk/water_merchant_fat.png"
+
+            // "creatures/caste_24.bmp"
+            // "bucklerparry_largerfolk/farmer1_fat.png"
